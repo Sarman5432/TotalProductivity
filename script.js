ba8408444c3086c5ece28604be238167e48be12c
Sarman <saulakh@kenna.ca>
2020-01-30 15:03:22 -0500

    get weathere data using jquery

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
 //API Keys
 const weatherKey = "33513f08e0c240ce0c03d54749e0a333"; //Dark Sky API
 
-getLocation(); //TODO: make it on load
-
-var weatherData; //data from Dark Sky Api as parsed json
-
-//add the weather to the UI
-function weatherUI(position) {
-    weatherData = loadWeatherData(weatherKey, position.coords.latitude, position.coords.longitude);
-    
-    //adding icon to grid 1
-    const weatherVisual = document.getElementsByClassName('weatherVisual');
-    const weatherImage = document.createElement('img');
-    const weatherLocation = document.createElement('p');
-    weatherImage.src = data.currently.icon;
-    weatherLocation.innerText = data.timezone;
-    weatherVisual.append(weatherImage);
-    weatherVisual.append(weatherLocation);
-}
+getLocation();
 
 //Gets User Location
-function getLocation(){
-    if(navigator.geolocation){
+function getLocation() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(weatherUI);
-    }else{
+    } else {
         alert('Geolocation is not supported by this browser');
     }
 }
 
-//Expands the section on button click
-function fullPage(){
-    const section = document.getElementsByClassName('notes');
-    
+
+//add the weather to the UI
+function weatherUI(position) {
+    var dataJSON;
+    //call API and store info - .getJSON is async operation
+    $.getJSON(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${weatherKey}/${position.coords.latitude},${position.coords.longitude}`, function (forecast) {
+        dataJSON = forecast;
+    });
+    console.log(dataJSON);
 }
 
-//Load weather
-function loadWeatherData(weatherKey, latitude, longitude){
-    var request = new XMLHttpRequest();
-    request.open('GET', `https://api.darksky.net/forecast/${weatherKey}/${latitude},${longitude}`, true)
-    
-    var data = JSON.parse(this.response)
+//Expands the section on button click
+function fullPage() {
+    const section = document.getElementsByClassName('notes');
 
+}
+
+
+
+
+//How to call api without jquery
+/*
+var request = new XMLHttpRequest();
+    request.open('GET', `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${weatherKey}/${position.coords.latitude},${position.coords.longitude}`, true)
     request.onload = function(){
+         var data = JSON.parse(this.response)
         if(request.status>=200 && request.status<400){
             console.log(data);
         }else{
@@ -49,20 +45,17 @@ function loadWeatherData(weatherKey, latitude, longitude){
         }
     }
     request.send();
-    return data
-}
+*/
 
-
-
-
-
-/*//practice getting json from var
+//practice getting json from var
+/*
 var data = '[ { "name": "Aragorn", "race": "Human" }, { "name": "Gimli", "race": "Dwarf" } ]'
 data = JSON.parse(data);
 console.log(data[0].name)
 */
 
-/*//practice getting json from json file/
+//practice getting json from json file
+/*
 var request = new XMLHttpRequest();
 request.open('GET', 'practice.json', true);
 
